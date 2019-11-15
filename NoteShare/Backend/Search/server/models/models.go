1f62833ee00e32d95e513b14b0abbbe1d9635c45
prachichouksey <prachi.chouksey@sjsu.edu>
2019-11-14 23:38:35 -0800

    Search functionality base code

---
+++ NoteShare/Backend/Search/server/models/models.go
@@ -0,0 +1,34 @@
+package models
+
+import ("go.mongodb.org/mongo-driver/bson/primitive"
+		"time"
+
+)
+type User struct {
+	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Userid string `bson:"userid" json:"userid"`
+	Firstname string `bson:"firstname" json:"firstname"`
+	Lastname string `bson:"lastname" json:"lastname"`
+	Username string `bson:"username" json:"username"`
+	Email string `bson:"email" json:"email"`
+	Phone string `bson:"phone" json:"phone"`
+	Tweets []Tweet `bson:"tweets"`
+	Followee []User `bson:"followee"`
+	CreatedAt time.Time `bson:"time" json:"time"`
+}
+
+type Tweet struct {
+	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Userid primitive.ObjectID `bson:"userid" json:"userid"`
+	Msg    string `bson:"msg" json:"msg"`
+	Created time.Time `bson:"time" json:"time"`
+}
+
+type Follow struct {
+	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Userid []primitive.ObjectID `bson:"userid" json:"userid"`
+}
+
+type Msg struct {
+	Msg   string `bson:"msg" json:"msg"`
+}
